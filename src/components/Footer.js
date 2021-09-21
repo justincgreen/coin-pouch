6e107f503d16ba0c65bee8a5cae177134a26c73d
Justin Green <justingreenmedia@gmail.com>
2021-09-21 12:33:43 -0500

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 			<div className="container text-center">
 				Powered by <a href="https://www.coingecko.com/en/api" target="_blank" rel="noreferrer">CoinGecko API</a>
 				<br />
-				&copy; 2021 <a href="https://www.justingreen.dev/" target="_blank" rel="noreferrer">Justin Green</a>
+				Built by <a href="https://www.justingreen.dev/" target="_blank" rel="noreferrer">Justin Green</a>
 			</div>
 		</footer>
 	)
